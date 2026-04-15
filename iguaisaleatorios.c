6704b99ca35aae5393ae1e939a56fb83def0222b
valentinaandradep <valentinaandradedepedro1@gmail.com>
2026-04-15 19:54:10 -0300

    Add files via upload

---
+++ iguaisaleatorios.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() 
+{
+    int num1, num2;
+    int tentativas = 0;
+
+    srand(time(NULL));
+
+    do 
+    {
+        num1 = (rand() % 1000) + 1;
+        num2 = (rand() % 1000) + 1;
+        tentativas++;
+    } 
+    while (num1 != num2);
+    printf("Após %d tentativas, os dois números gerados são iguais a %d\n", tentativas, num1);
+
+    return 0;
+}
